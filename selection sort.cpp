4614d3b3a7ad8accd1d8925dc8d7f708419ea95e
Ayush Thakur <113970894+ayushthakur04@users.noreply.github.com>
2023-02-09 15:55:17 +0530

    Create selection sort.cpp

---
+++ selection sort.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+void printvector(vector<int>a)
+{
+    for(int i;i<a.size();i++)
+    {
+        cout<<a[i]<<" ";
+    }
+    cout<<"\n";
+    
+}
+int getindexofminelement(vector<int>a,int i)
+{
+    int min_index=i;
+    for(;i<a.size();i++)
+    {
+        if(a[i]<a[min_index])
+        {
+            min_index=i;
+        }
+    }
+    return min_index;
+}
+void sorting(vector<int>&a)
+{
+    for(int i=0;i<a.size();i++)
+    {
+        int j=getindexofminelement(a,i);
+        swap(a[i],a[j]);
+        //cout<<" i : "<<i<<"vector : "
+       
+    }
+}
+int main()
+{
+    vector<int>a={6,1,2,3,4,6,1};
+    
+    sorting(a);
+    cout<<"sorted vector :  ";
+    printvector(a);
+    return 0;
+}
